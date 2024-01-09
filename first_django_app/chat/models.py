917f97da72134fc159ec1e7d64d232de6da652a1
Kai Wiehe <kai.wiehe@11880.com>
2024-01-09 15:32:17 +0100

    chat messages and DB

--- first_django_app/chat/models.py
+++ first_django_app/chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import date
+from django.conf import settings
 
 # Create your models here.
+
+class Chat(models.Model):
+    created_at = models.DateField(default=date.today)
+
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_at = models.DateField(default=date.today)
+    # chat = 
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_messaae_set', default=None, blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_messaae_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_messaae_set')
--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Message, Chat
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        print("received data " + request.POST["textmessage"])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST["textmessage"], chat=myChat, author=request.user, receiver=request.user)
+
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {"messages": chatMessages})
